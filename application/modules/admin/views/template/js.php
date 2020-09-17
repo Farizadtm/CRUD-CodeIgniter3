0ee776bad5a97221fa9d251c33bcdda325694f04
Farizadtm <farizadtm27@gmail.com>
2020-09-18 06:10:00 +0700

    templating

---
+++ application/modules/admin/views/template/js.php
@@ -0,0 +1,9 @@
+<script src="<?php echo base_url() ?>assets/js/feather-icons/feather.min.js"></script>
+<script src="<?php echo base_url() ?>assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<script src="<?php echo base_url() ?>assets/js/app.js"></script>
+
+<script src="<?php echo base_url() ?>assets/vendors/chartjs/Chart.min.js"></script>
+<script src="<?php echo base_url() ?>assets/vendors/apexcharts/apexcharts.min.js"></script>
+<script src="<?php echo base_url() ?>assets/js/pages/dashboard.js"></script>
+
+<script src="<?php echo base_url() ?>assets/js/main.js"></script>
